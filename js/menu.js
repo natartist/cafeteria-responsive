ba93016a291e25b5635a2a22a5f8ffcb58393d7a
natartist <nata.araya@gmail.com>
2021-12-10 21:59:42 -0300

    responsive celular menu

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-let burgerButton = document.getElementById(’burger-button’);
-let menu = document.getElementById(’menu‘);
+let burgerButton = document.getElementById("burger-button");
+let menu = document.getElementById("menu");
 
 //burgerButton contiene el span con el icono
 //burgerButton.addEventListener(‘que evento‘, ‘que deseo hacer‘);
-burgerButton.addEventListener(‘touchstart‘, function() {
-  menu.classList.toggle (‘active’);
-}
+burgerButton.addEventListener("touchstart", function() {
+  menu.classList.toggle("active");
+})
